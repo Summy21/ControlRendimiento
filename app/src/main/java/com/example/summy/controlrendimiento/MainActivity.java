d9e1695ab35873b31a695e653b2bfed8d4786ebe
Eneida Gonzales <eneidita21@gmail.com>
2017-09-29 16:25:49 -0300

    creacion de listener para autenticacion

--- app/src/main/java/com/example/summy/controlrendimiento/MainActivity.java
+++ app/src/main/java/com/example/summy/controlrendimiento/MainActivity.java
@@ -1,26 +1,125 @@
 package com.example.summy.controlrendimiento;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private Button crearcuentaButton;
+    private Button iniciarSesionButton;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //blindiar
+        crearcuentaButton = (Button)findViewById(R.id.crearcuentaButton);
+        iniciarSesionButton = (Button)findViewById(R.id.iniciarSesionButton);
+
+        emailEditText = (EditText)findViewById(R.id.emailEditText);
+        passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+
+        inicialize(); //inicializara los objetos de la parte superior
+
+        crearcuentaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                crearCuenta(emailEditText.getText().toString(), passwordEditText.getText().toString());
+            }
+        });
+
+        passwordEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                iniciarSesion(emailEditText.getText().toString(), passwordEditText.getText().toString());
+            }
+        });
+    }
+
+    private void inicialize() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {          //<--authlistener es donde detecta que hubo cambios en la sesion
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();  //FirebaseUser agarramos todos los gatos del usuario de la autenticacion una ves que ocurrio
+                if (firebaseUser != null){
+                    Log.w(TAG, "onAuthStateChanged - logeado" + firebaseUser.getUid());              //onAuthStateChanged<--proviene de que el estado cambio y si es != null entonces trago un usuario y esta logeado
+                    Log.w(TAG, "onAuthStateChanged - logeado" + firebaseUser.getEmail());
+                }else {
+                    Log.w(TAG, "onAuthStateChanged - cerro sesion");
+                }
+            }
+        };
     }
 
 
-    public void crearCuenta(View view) {
-        Intent intent = new Intent(this, RegistroActivity.class);
-        this.startActivity(intent);
+    private void crearCuenta(String email, String pass) {
+
+        firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Creacion de cuenta exitosa", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Creacion de cuenta no exitosa", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+      /*  Intent intent = new Intent(this, RegistroActivity.class);
+        this.startActivity(intent);*/
     }
 
-    public void iniciarSesion(View view) {
-        Intent intent = new Intent(this, DiariaActivity.class);
-        this.startActivity(intent);
+    private void iniciarSesion(String email, String pass) {
+
+        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Autenticacion exitosa", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Autenticacion no exitosa", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+     /*   Intent intent = new Intent(this, DiariaActivity.class);
+        this.startActivity(intent);*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
     }
 }
--- app/src/main/java/com/example/summy/controlrendimiento/RegistroActivity.java
+++ app/src/main/java/com/example/summy/controlrendimiento/RegistroActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by SUMMY on 28/9/2017.
  */
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private TextView detalleUserTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        detalleUserTextView = (TextView)findViewById(R.id.detalleUserTextView);
     }
 
     public void finalizarRegistro(View view) {
