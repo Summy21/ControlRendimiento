5c837b251cd01b23d6d4f728c0a71f6e4b3d244f
Eneida Gonzales <eneidita21@gmail.com>
2017-11-01 01:50:58 -0300

    implementacion de layout activity_obtener_competencias_api en XML, implementacion del item_ompetenciaApi en XML

--- app/src/main/java/com/example/summy/controlrendimiento/RegistroAdminActivity.java
+++ app/src/main/java/com/example/summy/controlrendimiento/RegistroAdminActivity.java
@@ -1,9 +1,13 @@
 package com.example.summy.controlrendimiento;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,10 +25,18 @@ public class RegistroAdminActivity extends AppCompatActivity {
 
     private static final String TAG = "RegistroAdminActivity";
 
+    //herramienta para crer el toolbar
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_admin);
+
+        //Vilculamos nuestro toolbar con el XML
+        toolbar = (Toolbar) findViewById(R.id.tvMenu);
+        toolbar.setTitle("Control Rendiminto");
+        setSupportActionBar(toolbar);
 
         detalleAdminTextView = (TextView)findViewById(R.id.detalleAdminTextView);
         cerrarSesionButton = (Button) findViewById(R.id.cerrarSesionButton);
@@ -38,6 +50,26 @@ public class RegistroAdminActivity extends AppCompatActivity {
                 cerrarSesion();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item2:
+                startActivity(new Intent(getApplicationContext(),ObtenerCompetenciasApiActivity.class));
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.item3:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void inicialize() {
